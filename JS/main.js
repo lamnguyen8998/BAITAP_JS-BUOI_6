2ca8c2798d29438b888775f2a46bcc5f91bd9506
lamnguyen8998 <84464387+lamnguyen8998@users.noreply.github.com>
2021-07-12 14:53:49 +0700

    update bai JS 6

--- JS/main.js
+++ JS/main.js
@@ -68,10 +68,10 @@ function taoTheDiv10() {
      for (var i = 0; i < divs.length; i++){
                    
         if ((i + 1) % 2 == 0){
-            divs[i].style.background = "red";
+            divs[i].style.background = "blue";
                     }
         else {
-             divs[i].style.background = "blue";
+             divs[i].style.background = "red";
               }
             }
 
